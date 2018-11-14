9c5a8aa8bafe214d169593f35f1a521e59cb47c5
Guillermo Bonafonte <guillermo.bonafontecriado@cognizant.com>
2018-11-14 10:27:18 +0100

    Added navigation using action

--- app/src/main/java/app/navigation/jetpack/guillermo/example/com/navigationapp/FormFragment.kt
+++ app/src/main/java/app/navigation/jetpack/guillermo/example/com/navigationapp/FormFragment.kt
@@ -1,11 +1,12 @@
 package app.navigation.jetpack.guillermo.example.com.navigationapp
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_form.*
 
 /**
  * A simple [Fragment] subclass.
@@ -13,9 +14,20 @@ import android.view.ViewGroup
  */
 class FormFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_form, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        floating_button_confirm_transaction.setOnClickListener {
+            val action = FormFragmentDirections.actionFormFragmentToSubmitFragment()
+//            action.setConceptArg("test")
+            findNavController().navigate(action)
+
+        }
     }
 }
